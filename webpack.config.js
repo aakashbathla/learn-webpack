be8aeedeb7b00318836690f47356753e36082a1b
aakashbathla <aakash.bathla7@gmail.com>
2020-06-02 20:26:55 +0530

    automatic server refresh

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,14 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
+  mode: "development",
   entry: {
     app: "./src/index.js",
-    print: "./src/print.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -15,5 +20,6 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
 };
