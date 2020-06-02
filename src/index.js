b620fbc10b4c602e5ae01dc5e7c6b9e1457b1420
aakashbathla <aakash.bathla7@gmail.com>
2020-06-02 19:37:41 +0530

    laoding assets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import _ from "lodash";
+import "./style.css";
+import Img from "./myphoto.jpg";
 function component() {
   const element = document.createElement("div");
   element.innerHTML = _.join(["Hello", "Webpack"], "");
+  element.classList.add("hello");
+  const myImage = new Image();
+  myImage.src = Img;
+  element.appendChild(myImage);
   return element;
 }
 document.body.appendChild(component());
